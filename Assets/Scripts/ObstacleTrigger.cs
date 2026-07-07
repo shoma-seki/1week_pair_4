5d1e77cd4465af561f474a9630a472dbe358282f
KanasashiHaruna <146906418+KanasashiHaruna@users.noreply.github.com>
2026-07-07 16:26:46 +0900

    飛んでくる障害物できました

---
+++ Assets/Scripts/ObstacleTrigger.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ObstacleTrigger : MonoBehaviour
+{
+    [SerializeField] private Obstacle obstacle;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Debug.Log("‚ ‚½‚½");
+            obstacle.StartMove(other.transform);
+        }
+    }
+}
