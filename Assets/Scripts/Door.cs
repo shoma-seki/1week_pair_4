d646237b41755f1de91beffcad4d007079ba2b11
KanasashiHaruna <146906418+KanasashiHaruna@users.noreply.github.com>
2026-07-08 11:25:46 +0900

    どあ

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -18,6 +18,7 @@ public class Door : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //Debug.Log("Door Start");
         leftClosedPos = leftDoor.localPosition;
         rightClosedPos = rightDoor.localPosition;
 
